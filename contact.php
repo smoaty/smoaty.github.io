2bdc4b33f73a375cb01468edbf05d127562b2995
Steve Moats <steven.moats@gmail.com>
2018-03-20 08:57:49 -0400

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,66 +1,14 @@
 <?php
 
-// hide all basic notices from PHP
-error_reporting(E_ALL ^ E_NOTICE); 
-
-if( isset($_POST['msg-submitted']) ) {
-	$name = $_POST['name'];
-	$email = $_POST['email'];
-	$subject = $_POST['subject'];
-	$message = $_POST['message'];
-
-	// server validation
-	if( trim($name) === '' ) {
-		$nameError = 'Please provide your name.';
-		$hasError = true;
-	}
-
-	if( trim($email) === '' ) {
-		$emailError = 'Please provide your email address.';
-		$hasError = true;
-	} else if( !preg_match("/^[[:alnum:]][a-z0-9_.-]*@[a-z0-9.-]+\.[a-z]{2,4}$/i", trim($email)) ) {
-		$emailError = 'Please provide valid email address.';
-		$hasError = true;
-	}
-
-	if( trim($message) === '' ) {
-		$messageError = "Please provide your message.";
-		$hasError = true;
-	} else {
-		if( function_exists( 'stripslashes' ) ) {
-			$message = stripslashes( trim( $message ) );
-		}
-	}
-		
-	if(!isset($hasError)) {
-		
-		$emailTo = 'steven.moats@gmail.com';
-		$subject = 'New Submitted Message From: ' . $name;
-		$body = "Name: $name \n\nEmail: $email \n\nMessage: $message";
-		$headers = 'From: ' .' <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $email;
-
-		mail($emailTo, $subject, $body, $headers);
-		
-		$message = 'Thank you ' . $name . ', your message has been submitted.';
-		$result = true;
-	
-	} else {
-
-		$arrMessage = array( $nameError, $emailError, $messageError );
-
-		foreach ($arrMessage as $key => $value) {
-			if( !isset($value) )
-				unset($arrMessage[$key]);
-		}
-
-		$message = implode( '<br/>', $arrMessage );
-		$result = false;
-	}
-
-	header("Content-type: application/json");
-	echo json_encode( array( 'message' => $message, 'result' => $result ));
-	die();
-}
-
+if ($_POST["email"]<>'') { 
+    $ToEmail = 'steven.moats@gmail.com'; 
+    $EmailSubject = 'Site contact form'; 
+    $mailheader = "From: ".$_POST["email"]."\r\n"; 
+    $mailheader .= "Reply-To: ".$_POST["email"]."\r\n"; 
+    $mailheader .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
+    $MESSAGE_BODY = "Name: ".$_POST["name"].""; 
+    $MESSAGE_BODY .= "Email: ".$_POST["email"].""; 
+    $MESSAGE_BODY .= "Comment: ".nl2br($_POST["message"]).""; 
+    mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Failure");
 
 ?>
